163768b10f57fd2de8f495d57150866f5ffde101
ErikBuer <buer94@gmail.com>
2021-04-17 16:27:44 +0200

    Updated metadata

---
+++ src/vector.rs
@@ -0,0 +1,21 @@
+//--------------------------------------------------------------//
+// Copyright 2021 Norsk Datateknikk AS                          //
+//--------------------------------------------------------------//
+// This file is subject to the terms and conditions defined in  //
+// file 'LICENSE', which is part of this source code package.   //
+//--------------------------------------------------------------//
+
+use super::sfunc::*;
+use super::cvfunc::*;
+
+/// Numeric vector type
+struct VectorF32
+{
+    obj:Vec<f32>
+}
+
+/// Numeric vector type
+struct VectorC32F
+{
+    obj:Vec<C32F>
+}
